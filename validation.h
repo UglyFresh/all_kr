c455e0992594f5550b2cc8c9fce3c4da30164f6f
UgluFresh <vlad12kuzmin@gmail.com>
2024-10-23 00:23:45 +0300

    Правки в функции ValidDateInput

--- validation.h
+++ validation.h
@@ -3,6 +3,6 @@
 /// <summary> Прроверка пользовательского ввода </summary>/// <returns>Пользовательский ввод</returns>
 int ValidUserInput();
 /// <summary> Проверка корректности даты </summary>/// <returns>Экземпляр класса Date</returns>
-Date ValidDateInput(std::vector <Date> _date);
+Date ValidDateInput(std::vector <Date>& _date);
 /// <summary> Проверка имении файла на зарезервированность </summary>/// <param name="file_name"> Путь к файлу </param>/// <returns> Истинна или Ложь </returns>
 bool ValidFileName(std::string file_name);

7b6f37842801108625b9fbd27e3eeff89e746f5a
UgluFresh <vlad12kuzmin@gmail.com>
2024-11-20 21:28:01 +0300

    Добавлена функция IsValidFileName

--- kr2_inheritance/validation.cpp
+++ kr2_inheritance/validation.cpp
@@ -1,4 +1,5 @@
 #include "validation.h"
+#include <vector>
 
 int ValidUserInput() {
 
@@ -22,5 +23,27 @@ int ValidChoice(int begin, int finish) {
     }
 
     return menu_choice;
+
+}
+
+bool IsValidFileName(const std::string file_name) {
+
+	std::vector<std::string> reserved_filenames = {
+		"CON.txt",  "PRN.txt",  "AUX.txt",  "NUL.txt",  "COM1.txt", "COM2.txt",
+		"COM3.txt", "COM4.txt", "COM5.txt", "COM6.txt", "COM7.txt", "COM8.txt",
+		"COM9.txt", "LPT1.txt", "LPT2.txt", "LPT3.txt", "LPT4.txt", "LPT5.txt",
+		"LPT6.txt", "LPT7.txt", "LPT8.txt", "LPT9.txt", "con.txt",  "prn.txt",
+		"aux.txt",  "nul.txt",  "com1.txt", "com2.txt", "com3.txt", "com4.txt",
+		"com5.txt", "com6.txt", "com7.txt", "com8.txt", "com9.txt", "lpt1.txt",
+		"lpt2.txt", "lpt3.txt", "lpt4.txt", "lpt5.txt", "lpt6.txt", "lpt7.txt",
+		"lpt8.txt", "lpt9.txt" };
+
+	for (const std::string& reserved_filename : reserved_filenames) {
+		if (file_name.find(reserved_filename) != std::string::npos) {
+			return true;
+		}
+	}
+
+	return false;
 
 }
--- kr2_inheritance/validation.h
+++ kr2_inheritance/validation.h
@@ -14,3 +14,10 @@ int ValidUserInput();
 /// <param name="finish">Верхняя граница выбора</param>
 /// <returns>Введенное значение</returns>
 int ValidChoice(int begin, int finish);
+
+/// <summary>
+/// Проверка корректности имени файла
+/// </summary>
+/// <param name="file_name">Путь к файлу</param>
+/// <returns>Истина или ложь</returns>
+bool IsValidFileName(const std::string file_name);

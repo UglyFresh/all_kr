2d13825963f70389584519a85a34c3eea3a10fed
UgluFresh <vlad12kuzmin@gmail.com>
2024-11-24 17:36:03 +0300

    Добавлен метод InputData

--- kr2_inheritance/person.h
+++ kr2_inheritance/person.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <iostream>
+#include "validation.h"
 
 
 /// <summary>
@@ -9,20 +9,20 @@ class Person {
 protected:
 
 	std::string surname;
-	char gender;
+	std::string gender;
 	std::string address;
 
 public:
 
 	Person() = default;
-	Person(std::string srnm, char gndr, std::string adrs):surname(srnm), gender(gndr), address(adrs){}
+	Person(std::string srnm, std::string gndr, std::string adrs):surname(srnm), gender(gndr), address(adrs){}
 
 	std::string get_surname() const { return surname; }
-	char get_gender() const { return gender; }
+	std::string get_gender() const { return gender; }
 	std::string get_address() const { return address; }
 
 	void set_surname(const std::string& new_surname) { surname = new_surname; }
-	void set_gender(const char& new_gender) { gender = new_gender; }
+	void set_gender(const std::string& new_gender) { gender = new_gender; }
 	void set_address(const std::string& new_address) { address = new_address; }
 
 	virtual void Show() const {
@@ -32,7 +32,26 @@ public:
 			<< "Адрес: " << get_address() << ';' << std::endl;
 	}
 
-	~Person() = default;
+	virtual std::vector<std::unique_ptr<Person>> InputData() {
+
+		std::vector <std::unique_ptr<Person>> person_info;
+
+		std::cout << "Введите фамилию: ";
+		std::cin >> surname;
+		std::cout << "Введите пол (мужской - M/женский - W): ";
+		std::cin >> gender;
+		while (gender != "M" && gender != "W") {
+			std::cout << "Ошибка! Выберите из предложенных вариантов: ";
+			std::cin >> gender;
+		}
+		std::cout << "Введите адрес: ";
+		std::cin >> address;
+
+		person_info.push_back(std::make_unique<Person>(surname, gender, address));
+		return person_info;
+	}
+
+	virtual ~Person() {}
 
 };
 
@@ -49,7 +68,7 @@ private:
 public:
 
 	Employee() = default;
-	Employee(std::string srnm, char gndr, std::string adrs, std::string dprtmnt, std::string sbjct) : Person(srnm, gndr, adrs), department(dprtmnt), subject(sbjct){}
+	Employee(std::string srnm, std::string gndr, std::string adrs, std::string dprtmnt, std::string sbjct) : Person(srnm, gndr, adrs), department(dprtmnt), subject(sbjct){}
 
 	std::string get_department() const { return department; }
 	std::string get_subject() const { return subject; }
@@ -66,6 +85,29 @@ public:
 			<< "Должность" << get_subject() << ';' << std::endl;
 	}
 
-	~Employee() = default;
+	virtual std::vector<std::unique_ptr<Person>> InputData() override {
+
+		std::vector <std::unique_ptr<Person>> person_info;
+
+		std::cout << "Введите фамилию: ";
+		std::cin >> surname;
+		std::cout << "Введите пол (мужской - M/женский - W): ";
+		std::cin >> gender;
+		while (gender != "M" && gender != "W") {
+			std::cout << "Ошибка! Выберите из предложенных вариантов: ";
+			std::cin >> gender;
+		}
+		std::cout << "Введите адрес: ";
+		std::cin >> address;
+		std::cout << "Введите кафедру: ";
+		std::cin >> department;
+		std::cout << "Введите предмет который ведет сотрудник: ";
+		std::cin >> subject;
+
+		person_info.push_back(std::make_unique<Employee>(surname, gender, address, department, subject));
+		return person_info;
+	}
+
+	virtual ~Employee() override {};
 
 };
